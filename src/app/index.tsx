e1a9fbe270b7bef6526d308817e3d1a67ab9e72c
Yuka <yoz.peridot.738@gmail.com>
2024-07-28 23:12:54 +0900

    initial commit

---
+++ src/app/index.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+
+const Index = (): JSX.Element => {
+    return (
+        <View>
+            <Text>
+                Hello World
+            </Text>
+        </View>
+    )
+}
+
+export default Index
