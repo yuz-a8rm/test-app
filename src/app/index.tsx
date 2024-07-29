a6e9a0408581014ad2d51ad496001382f62de602
Yuka <yoz.peridot.738@gmail.com>
2024-07-29 11:44:50 +0900

    11-60まで

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,53 +1,23 @@
 import { View, Text, StyleSheet } from "react-native";
 
+import Header from "../components/header";
+import MemoListItem from "../components/MemoListItem";
+import CircleButton from "../components/CircleButton";
+
 const Index = (): JSX.Element => {
     return (
         <View style={styles.container}>
 
-           <View style={styles.header}>
-                <View style={styles.headerInner}>
-                    <Text style={styles.headerTitle}>Memo App</Text>
-                    <Text style={styles.headerRight}>ログアウト</Text>
-                </View>
-            </View>
+        <Header />
 
-            <View>
+        <View>
+            <MemoListItem />
+            <MemoListItem />
+            <MemoListItem />
+        </View>
 
-                <View style={styles.memoListItem}>
-                    <View>
-                        <Text style={styles.memoListItemTitle}>買い物リスト</Text>
-                        <Text style={styles.memoListItemDate}>2024年7月28日</Text>
-                    </View>
-                    <View>
-                        <Text>X</Text>
-                    </View>
-                </View>
+        <CircleButton>+</CircleButton>
 
-                <View style={styles.memoListItem}>
-                    <View>
-                        <Text style={styles.memoListItemTitle}>買い物リスト</Text>
-                        <Text style={styles.memoListItemDate}>2024年7月28日</Text>
-                    </View>
-                    <View>
-                        <Text>X</Text>
-                    </View>
-                </View>
-
-                <View style={styles.memoListItem}>
-                    <View>
-                        <Text style={styles.memoListItemTitle}>買い物リスト</Text>
-                        <Text style={styles.memoListItemDate}>2024年7月28日</Text>
-                    </View>
-                    <View>
-                        <Text>X</Text>
-                    </View>
-                </View>
-
-            </View>
-
-            <View>
-                <Text>+</Text>
-            </View>
         </View>
     )
 }
@@ -57,47 +27,8 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: '#ffffff'
     },
-    header: {
-        backgroundColor: '#467FD3',
-        height: 104,
-        justifyContent: 'flex-end'
-    },
-    headerInner: {
-        alignItems: 'center',
-    },
-    headerRight: {
-        position: 'absolute',
-        right: 16,
-        bottom: 16,
-        color: 'rgba(255,255,255,0.7)'
-    },
-    headerTitle: {
-        marginBottom: 8,
-        fontSize: 22,
-        lineHeight: 32,
-        fontWeight: 'bold',
-        color: '#ffffff'
-        
-    },
-    memoListItem :{
-        backgroundColor: '#ffffff',
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        paddingVertical: 16,
-        paddingHorizontal: 19,
-        alignItems: 'center',
-        borderBottomWidth: 1,
-        borderColor: 'rgba(0,0,0,0.15)'
-    },
-    memoListItemTitle :{
-        fontSize: 16,
-        lineHeight: 32
-    },
-    memoListItemDate :{
-        fontSize: 12,
-        lineHeight: 16,
-        color: '#848484'
-    }
+   
+
 
 })
 
