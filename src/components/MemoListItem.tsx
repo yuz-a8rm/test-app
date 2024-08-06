8e22feb6ec5f40e5b4e8b438852ed8287a587e0e
Yuka <yoz.peridot.738@gmail.com>
2024-08-06 14:35:56 +0900

    13-90まで

--- src/components/MemoListItem.tsx
+++ src/components/MemoListItem.tsx
@@ -1,16 +1,25 @@
 import { View, TouchableOpacity, Text, StyleSheet } from "react-native";
-
 import { Link } from "expo-router";
 
 import Icon from "./icon";
+import { type Memo } from "../../types/memo";
 
-const MemoListItem = (): JSX.Element => {
+interface Props {
+    memo: Memo
+}
+
+const MemoListItem = (props: Props): JSX.Element | null => {
+    const { memo } = props
+    const { bodyText, updatedAt } = memo
+    if ( bodyText === null || updatedAt === null) { return null}
+    const  dateString = updatedAt.toDate().toLocaleString('ja-JP')
+
     return (
         <Link href='/memo/detail' asChild>
             <TouchableOpacity style={styles.memoListItem}>
                 <View>
-                    <Text style={styles.memoListItemTitle}>買い物リスト</Text>
-                    <Text style={styles.memoListItemDate}>2024年7月28日</Text>
+                    <Text numberOfLines={1} style={styles.memoListItemTitle}>{bodyText}</Text>
+                    <Text style={styles.memoListItemDate}>{dateString}</Text>
                 </View>
                 <TouchableOpacity>
                     <Icon name='delete' size={32} color='#b0b0b0' />
