df5b5d35d1bf75f0589532e172cc72d444d06736
Yuka <yoz.peridot.738@gmail.com>
2024-08-10 18:46:32 +0900

    最後まで

--- src/app/memo/edit.tsx
+++ src/app/memo/edit.tsx
@@ -1,20 +1,59 @@
 import { 
-    View, StyleSheet, TextInput, KeyboardAvoidingView } from "react-native";
+    View, StyleSheet, TextInput, Alert} from "react-native";
 import CircleButton from "../../components/CircleButton";
 import Icon from "../../components/icon";
-import { router } from "expo-router";
+import { router, useLocalSearchParams } from "expo-router";
+import { useState, useEffect } from "react";
+import { doc, getDoc, setDoc, Timestamp } from "firebase/firestore";
+import KeyboardAvoidingView from "../../components/KeyBoardAvoidingView"
 
-const handlePress = (): void => {
-    router.back()
+import { auth, db } from "../../config";
+
+const handlePress = (id: string, bodyText: string): void => {
+    if (auth.currentUser == null ) { return }
+    const ref = doc(db, `users/${auth.currentUser.uid}/memos`, id)
+    setDoc(ref, {
+        bodyText,
+        updatedAt: Timestamp.fromDate(new Date())
+    })
+    .then(() => {
+        router.back()
+    })
+    .catch((error) => {
+        console.log(error)
+        Alert.alert('更新に失敗しました')
+    })
+
 }
 
 const Edit = () => {
+    const id = String(useLocalSearchParams().id)
+    const [bodyText, setBodyText] = useState('')
+    useEffect(() => {
+        if (auth.currentUser === null) { return }
+        const ref = doc(db, `users/${auth.currentUser.uid}/memos`, id)
+    getDoc(ref)
+        .then((docRef) => {
+            const RemoteBodyText = docRef?.data()?.bodyText
+            setBodyText(RemoteBodyText)
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+    },[])
+
+    console.log('edit', id)
     return (
-        <KeyboardAvoidingView behavior='height' style={styles.container}>
+        <KeyboardAvoidingView style={styles.container}>
             <View style={styles.inputContainer}>
-                <TextInput multiline style={styles.input} value={'買い物\nリスト'} />
+                <TextInput 
+                multiline style={styles.input} 
+                value={bodyText}
+                onChangeText={(text) => { setBodyText(text) }}
+                autoFocus
+                />
             </View>
-            <CircleButton onPress={handlePress}>
+            <CircleButton onPress={() => {handlePress( id, bodyText )}}>
                 <Icon name='check' size={40} color='#ffffff'/>
             </CircleButton>
         
@@ -29,15 +68,16 @@ const styles = StyleSheet.create({
         backgroundColor: '#ffffff'
     },
     inputContainer: {
-        paddingVertical: 32,
-        paddingHorizontal: 27,
         flex: 1
     },
     input: {
         flex: 1,
         textAlignVertical: 'top',
         fontSize: 16,
-        lineHeight: 24
+        lineHeight: 24,
+        paddingVertical: 32,
+        paddingHorizontal: 27,
+
     }
 })
 
--- src/components/MemoListItem.tsx
+++ src/components/MemoListItem.tsx
@@ -1,11 +1,31 @@
-import { View, TouchableOpacity, Text, StyleSheet } from "react-native";
+import { View, TouchableOpacity, Text, StyleSheet, Alert } from "react-native";
 import { Link } from "expo-router";
+import { deleteDoc, doc } from "firebase/firestore";
 
 import Icon from "./icon";
 import { type Memo } from "../../types/memo";
+import { auth, db } from "../config";
 
 interface Props {
     memo: Memo
+}
+
+const handlePress = ( id: string):void => {
+    if (auth.currentUser === null) { return }
+    const ref = doc( db, `users/${auth.currentUser.uid}/memos` , id)
+    Alert.alert('メモを削除します', 'よろしいですか？', [
+        {
+            text: 'キャンセル'
+        },
+        {
+            text: '削除する',
+            style: 'destructive',
+            onPress: () => {
+                deleteDoc(ref)
+                .catch(() => Alert.alert('削除に失敗しました'))
+            }
+        }
+    ])
 }
 
 const MemoListItem = (props: Props): JSX.Element | null => {
@@ -23,7 +43,7 @@ const MemoListItem = (props: Props): JSX.Element | null => {
                     <Text numberOfLines={1} style={styles.memoListItemTitle}>{bodyText}</Text>
                     <Text style={styles.memoListItemDate}>{dateString}</Text>
                 </View>
-                <TouchableOpacity>
+                <TouchableOpacity onPress={() => { handlePress(memo.id) }}>
                     <Icon name='delete' size={32} color='#b0b0b0' />
                 </TouchableOpacity>
             </TouchableOpacity>
